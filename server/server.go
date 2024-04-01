3dbbabbbfa424b1b9bd55d60aaac16a130098027
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-04-01 14:12:34 +0100

    license: add license header

--- server/server.go
+++ server/server.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Andrew Dunstall. All rights reserved.
+//
+// Use of this source code is governed by a MIT style license that can be
+// found in the LICENSE file.
+
 package server
 
 import (
