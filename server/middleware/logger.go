d2dc582912bb89bce8a44db6a9750e25752e9fdb
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-04-04 05:14:32 +0100

    middleware: update log subsystem

--- server/middleware/logger.go
+++ server/middleware/logger.go
@@ -6,7 +6,6 @@
 package middleware
 
 import (
-	"strings"
 	"time"
 
 	"github.com/andydunstall/pico/pkg/log"
@@ -16,9 +15,7 @@ import (
 
 // NewLogger creates logging middleware that logs every request.
 func NewLogger(logger *log.Logger) gin.HandlerFunc {
-	// Use diffent subsystems to filter by pico and proxy requests.
-	picoLogger := logger.WithSubsystem("server.http.pico")
-	proxyLogger := logger.WithSubsystem("server.http.proxy")
+	logger = logger.WithSubsystem("server.http.route")
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
@@ -29,19 +26,14 @@ func NewLogger(logger *log.Logger) gin.HandlerFunc {
 		// Process request
 		c.Next()
 
-		fields := []zap.Field{
+		logger.Debug(
+			"http request",
 			zap.String("method", c.Request.Method),
 			zap.Int("status", c.Writer.Status()),
 			zap.String("path", path),
 			zap.Int64("latency", time.Since(start).Milliseconds()),
 			zap.String("client-ip", c.ClientIP()),
 			zap.Int("resp-size", c.Writer.Size()),
-		}
-		if strings.HasPrefix(c.Request.URL.Path, "/pico") {
-			picoLogger.Debug("http request", fields...)
-		} else {
-			// TODO(andydunstall): Add endpoint ID.
-			proxyLogger.Debug("http request", fields...)
-		}
+		)
 	}
 }
