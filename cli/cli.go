92dc0a640c98c4c1802c65fe75b276138ac4ce91
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-04-06 10:28:11 +0100

    repo: remove license on files

--- cli/cli.go
+++ cli/cli.go
@@ -1,8 +1,3 @@
-// Copyright 2024 Andrew Dunstall. All rights reserved.
-//
-// Use of this source code is governed by a MIT style license that can be
-// found in the LICENSE file.
-
 package cli
 
 func Start() error {
