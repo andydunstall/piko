006d9320f65b62e75212861b7242e2f5ee4d9f84
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-04-09 06:18:03 +0100

    netmap: add netmap

--- server/netmap/netmap.go
+++ server/netmap/netmap.go
@@ -1,41 +1,83 @@
 package netmap
 
+import "sync"
+
 // NetworkMap represents the known state of the cluster as seen by the local
 // node.
 //
 // This map is eventually consistent. The state is propagated among the nodes
 // in the cluster using gossip.
 type NetworkMap struct {
+	localID string
+	nodes   map[string]*Node
+
+	// mu protects the above fields.
+	mu sync.RWMutex
 }
 
-func NewNetworkMap(_ *Node) *NetworkMap {
-	return &NetworkMap{}
+func NewNetworkMap(localNode *Node) *NetworkMap {
+	nodes := make(map[string]*Node)
+	nodes[localNode.ID] = localNode
+	return &NetworkMap{
+		localID: localNode.ID,
+		nodes:   nodes,
+	}
 }
 
 func (m *NetworkMap) LocalNode() *Node {
-	return nil
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return m.nodes[m.localID]
 }
 
-func (m *NetworkMap) NodeByID(_ string) (*Node, bool) {
-	return nil, false
+func (m *NetworkMap) NodeByID(id string) (*Node, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	node, ok := m.nodes[id]
+	return node, ok
 }
 
-func (m *NetworkMap) UpdateNodeByID(_ string, _ func(n *Node)) bool {
-	return false
+func (m *NetworkMap) AddNode(node *Node) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.nodes[node.ID] = node
 }
 
-func (m *NetworkMap) DeleteNodeByID(_ string) bool {
-	return false
+// UpdateNodeByID updates the node with the given ID using a callback function.
+//
+// The function must not block as it is called while the netmap mutex is held.
+func (m *NetworkMap) UpdateNodeByID(id string, f func(n *Node)) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	node, ok := m.nodes[id]
+	if !ok {
+		return false
+	}
+	f(node)
+	return true
 }
 
-func (m *NetworkMap) AddNode(_ *Node) {
+func (m *NetworkMap) DeleteNodeByID(id string) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	_, ok := m.nodes[id]
+	delete(m.nodes, id)
+	return ok
 }
 
 func (m *NetworkMap) OnLocalStatusUpdated(_ func(status NodeStatus)) {
+	// TODO(andydunstall)
 }
 
 func (m *NetworkMap) OnLocalEndpointUpdated(_ func(endpointID string, numListeners int)) {
+	// TODO(andydunstall)
 }
 
 func (m *NetworkMap) OnLocalEndpointRemoved(_ func(endpointID string)) {
+	// TODO(andydunstall)
 }
