c4b59a08e0dbd439be686e6a5deec8d7c19eeb7d
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-05-06 09:18:10 +0100

    config: add auth config options

---
+++ server/auth/config.go
@@ -0,0 +1,73 @@
+package auth
+
+import (
+	"github.com/spf13/pflag"
+)
+
+type Config struct {
+	// TokenHMACSecretKey is the secret key to authenticate HMAC endpoint
+	// connection JWTs.
+	TokenHMACSecretKey string
+
+	// TokenRSAPublicKey is the public key to authenticate RSA endpoint
+	// connection JWTs.
+	TokenRSAPublicKey string
+
+	// TokenECDSAPublicKey is the public key to authenticate ECDSA endpoint
+	// connection JWTs.
+	TokenECDSAPublicKey string
+
+	// TokenAudience is the required 'aud' claim of the authenticated JWTs.
+	//
+	// If not given the 'aud' claim will be ignored.
+	TokenAudience string
+
+	// TokenIssuer is the required 'iss' claim of the authenticated JWTs.
+	//
+	// If not given the 'iss' claim will be ignored.
+	TokenIssuer string
+}
+
+func (c *Config) RegisterFlags(fs *pflag.FlagSet) {
+	fs.StringVar(
+		&c.TokenHMACSecretKey,
+		"auth.token-hmac-secret-key",
+		"",
+		`
+Secret key to authenticate HMAC endpoint connection JWTs.`,
+	)
+	fs.StringVar(
+		&c.TokenRSAPublicKey,
+		"auth.token-rsa-public-key",
+		"",
+		`
+Public key to authenticate RSA endpoint connection JWTs.`,
+	)
+	fs.StringVar(
+		&c.TokenECDSAPublicKey,
+		"auth.token-ecdsa-public-key",
+		"",
+		`
+Public key to authenticate ECDSA endpoint connection JWTs.`,
+	)
+	fs.StringVar(
+		&c.TokenAudience,
+		"auth.token-audience",
+		"",
+		`
+Audience of endpoint connection JWT token to verify.
+
+If given the JWT 'aud' claim must match the given audience. Otherwise it
+is ignored.`,
+	)
+	fs.StringVar(
+		&c.TokenIssuer,
+		"auth.token-issuer",
+		"",
+		`
+Issuer of endpoint connection JWT token to verify.
+
+If given the JWT 'iss' claim must match the given issuer. Otherwise it
+is ignored.`,
+	)
+}
