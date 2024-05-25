4e68c12e21561b98c3cddb5a0a0268bfce2104a2
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-05-25 15:17:09 +0100

    admin: add pprof endpoints
    Registers /debug/pprof endpoints on the admin server.

--- server/server/admin/server.go
+++ server/server/admin/server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"time"
 
 	"github.com/andydunstall/piko/pkg/forwarder"
@@ -112,6 +113,21 @@ func (s *Server) registerRoutes() {
 	if s.registry != nil {
 		s.router.GET("/metrics", s.metricsHandler())
 	}
+
+	// From https://github.com/gin-contrib/pprof/blob/934af36b21728278339704005bcef2eec1375091/pprof.go#L32.
+	pprofGroup := s.router.Group("/debug/pprof")
+	pprofGroup.GET("/", gin.WrapF(pprof.Index))
+	pprofGroup.GET("/cmdline", gin.WrapF(pprof.Cmdline))
+	pprofGroup.GET("/profile", gin.WrapF(pprof.Profile))
+	pprofGroup.POST("/symbol", gin.WrapF(pprof.Symbol))
+	pprofGroup.GET("/symbol", gin.WrapF(pprof.Symbol))
+	pprofGroup.GET("/trace", gin.WrapF(pprof.Trace))
+	pprofGroup.GET("/allocs", gin.WrapH(pprof.Handler("allocs")))
+	pprofGroup.GET("/block", gin.WrapH(pprof.Handler("block")))
+	pprofGroup.GET("/goroutine", gin.WrapH(pprof.Handler("goroutine")))
+	pprofGroup.GET("/heap", gin.WrapH(pprof.Handler("heap")))
+	pprofGroup.GET("/mutex", gin.WrapH(pprof.Handler("mutex")))
+	pprofGroup.GET("/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
 }
 
 func (s *Server) healthRoute(c *gin.Context) {
