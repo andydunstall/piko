b898e1080b95271a5076785352e015b70c615a0e
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-06-05 19:36:17 +0100

    serverv2: load balance among connected upstreams
    Load balances incoming proxy requests among connected upstreams in a
    round-robin fashion.

--- serverv2/upstream/manager.go
+++ serverv2/upstream/manager.go
@@ -1,57 +1,83 @@
 package upstream
 
 import (
-	"fmt"
-	"net"
 	"sync"
 )
 
+// localLoadBalancer load balances requests among upstreams connected to
+// the local node.
+type localLoadBalancer struct {
+	upstreams []Upstream
+	nextIndex int
+}
+
+func (lb *localLoadBalancer) Add(u Upstream) {
+	lb.upstreams = append(lb.upstreams, u)
+}
+
+func (lb *localLoadBalancer) Remove(u Upstream) bool {
+	for i := 0; i != len(lb.upstreams); i++ {
+		if lb.upstreams[i] != u {
+			continue
+		}
+		lb.upstreams = append(lb.upstreams[:i], lb.upstreams[i+1:]...)
+		if len(lb.upstreams) == 0 {
+			return true
+		}
+		lb.nextIndex %= len(lb.upstreams)
+		return false
+	}
+	return len(lb.upstreams) == 0
+}
+
+func (lb *localLoadBalancer) Next() Upstream {
+	if len(lb.upstreams) == 0 {
+		return nil
+	}
+
+	u := lb.upstreams[lb.nextIndex]
+	lb.nextIndex++
+	lb.nextIndex %= len(lb.upstreams)
+	return u
+}
+
 // Manager manages the set of local upsteam services.
 type Manager struct {
-	upstreams map[string][]Upstream
+	localLoadBalancers map[string]*localLoadBalancer
 
 	mu sync.Mutex
 }
 
 func NewManager() *Manager {
 	return &Manager{
-		upstreams: make(map[string][]Upstream),
+		localLoadBalancers: make(map[string]*localLoadBalancer),
 	}
 }
 
-func (m *Manager) Add(upstream Upstream) {
+func (m *Manager) Add(u Upstream) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	m.upstreams[upstream.EndpointID()] = append(
-		m.upstreams[upstream.EndpointID()], upstream,
-	)
-}
-
-func (m *Manager) Remove(upstream Upstream) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	var upstreams []Upstream
-	for _, u := range m.upstreams[upstream.EndpointID()] {
-		if u != upstream {
-			upstreams = append(upstreams, u)
-		}
+	lb, ok := m.localLoadBalancers[u.EndpointID()]
+	if !ok {
+		lb = &localLoadBalancer{}
 	}
 
-	m.upstreams[upstream.EndpointID()] = upstreams
+	lb.Add(u)
+	m.localLoadBalancers[u.EndpointID()] = lb
 }
 
-func (m *Manager) Dial(endpointID string) (net.Conn, error) {
+func (m *Manager) Remove(u Upstream) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	upstreams, ok := m.upstreams[endpointID]
-	if !ok || len(upstreams) == 0 {
-		return nil, fmt.Errorf("not found")
+	lb, ok := m.localLoadBalancers[u.EndpointID()]
+	if !ok {
+		return
 	}
-
-	return upstreams[0].Dial()
+	if lb.Remove(u) {
+		delete(m.localLoadBalancers, u.EndpointID())
+	}
 }
 
 func (m *Manager) Select(endpointID string, _ bool) (Upstream, bool) {
@@ -61,9 +87,9 @@ func (m *Manager) Select(endpointID string, _ bool) (Upstream, bool) {
 	// TODO(andydunstall): If allowForward can select from another node, where
 	// Dial is just TCP
 
-	upstreams, ok := m.upstreams[endpointID]
-	if !ok || len(upstreams) == 0 {
+	lb, ok := m.localLoadBalancers[endpointID]
+	if !ok {
 		return nil, false
 	}
-	return upstreams[0], true
+	return lb.Next(), true
 }
