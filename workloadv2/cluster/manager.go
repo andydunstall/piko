2e5aa68d44b7971d8e8ebc7b3f9c6793e20cd04d
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-06-30 07:53:53 +0100

    workloadv2: add nodes mutex

--- workloadv2/cluster/manager.go
+++ workloadv2/cluster/manager.go
@@ -1,6 +1,8 @@
 package cluster
 
 import (
+	"sync"
+
 	"github.com/andydunstall/piko/pkg/log"
 	"github.com/andydunstall/piko/workloadv2/cluster/config"
 	"go.uber.org/zap"
@@ -8,6 +10,8 @@ import (
 
 type Manager struct {
 	nodes []*Node
+
+	mu sync.Mutex
 
 	logger log.Logger
 }
@@ -28,32 +32,44 @@ func NewManager(opts ...Option) *Manager {
 func (m *Manager) Update(config *config.Config) {
 	m.logger.Info("update", zap.Any("config", config))
 
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	// Update the active nodes to ensure we have the correct number.
 	if config.Nodes > len(m.nodes) {
 		added := config.Nodes - len(m.nodes)
 		for i := 0; i != added; i++ {
-			m.addNode()
+			m.addNodeLocked()
 		}
 	} else if len(m.nodes) > config.Nodes {
 		removed := len(m.nodes) - config.Nodes
 		for i := 0; i != removed; i++ {
-			m.removeNode()
+			m.removeNodeLocked()
 		}
 	}
 }
 
 func (m *Manager) Nodes() []*Node {
-	return m.nodes
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// Copy nodes to avoid race conditions when m.nodes is updated.
+	var nodes []*Node
+	nodes = append(nodes, m.nodes...)
+	return nodes
 }
 
 func (m *Manager) Close() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	removed := len(m.nodes)
 	for i := 0; i != removed; i++ {
-		m.removeNode()
+		m.removeNodeLocked()
 	}
 }
 
-func (m *Manager) addNode() {
+func (m *Manager) addNodeLocked() {
 	m.logger.Info("add node")
 
 	var gossipAddrs []string
@@ -67,7 +83,7 @@ func (m *Manager) addNode() {
 	m.nodes = append(m.nodes, node)
 }
 
-func (m *Manager) removeNode() {
+func (m *Manager) removeNodeLocked() {
 	m.logger.Info("remove node")
 
 	// Remove the oldest node.
