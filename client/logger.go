4953f944b5193d6ac6020cb511d59b13a81614ae
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-07-03 08:06:08 +0100

    client: add upstream listen
    Adds support for 'upstream.Listen(...)'.

--- client/logger.go
+++ client/logger.go
@@ -1,6 +1,44 @@
 package client
 
-import "go.uber.org/zap"
+import (
+	"io"
+	"strings"
+
+	"go.uber.org/zap"
+)
+
+// yamuxLogWriter is an adapter to log yamux logs with the client logger.
+type yamuxLogWriter struct {
+	logger Logger
+}
+
+func (l *yamuxLogWriter) Write(b []byte) (int, error) {
+	s := string(b)
+	s = strings.TrimSpace(s)
+
+	_, after, found := strings.Cut(s, "[WARN] ")
+	if found {
+		l.logger.Warn(after)
+		return len(b), nil
+	}
+	_, after, found = strings.Cut(s, "[ERR] ")
+	if found {
+		l.logger.Error(after)
+		return len(b), nil
+	}
+
+	_, after, found = strings.Cut(s, "yamux: ")
+	if found {
+		l.logger.Error("yamux: " + after)
+		return len(b), nil
+	}
+
+	// Log unexpected formats as errors.
+	l.logger.Error(s)
+	return len(b), nil
+}
+
+var _ io.Writer = &yamuxLogWriter{}
 
 // Logger is a logger compatible with zap.Logger.
 type Logger interface {
