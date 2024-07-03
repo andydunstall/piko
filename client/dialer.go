e9caea3e47f776044785c8be648cfeff701f840d
Andrew Dunstall <andydunstall@hotmail.co.uk>
2024-07-03 08:13:55 +0100

    client: add dialer
    Adds support for 'dialer.Dial(...)'.
    
    Dial is simply a WebSocket to Piko that includes the endpoint ID in the
    request. This can then be used as a standard net.Conn.

--- client/dialer.go
+++ client/dialer.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"crypto/tls"
 	"net"
+	"net/url"
+
+	"github.com/andydunstall/piko/pkg/websocket"
 )
 
 // Dialer manages opening connections to upstream endpoints.
@@ -17,7 +20,7 @@ type Dialer struct {
 	//
 	// This must be the URL of the 'proxy' port. Defaults to
 	// 'http://localhost:8000'.
-	URL string
+	URL *url.URL
 
 	// TLSConfig specifies the TLS configuration to use with the Piko server.
 	//
@@ -27,6 +30,33 @@ type Dialer struct {
 
 // Dial opens a TCP connection to the endpoint with the given ID and
 // returns a [net.Conn].
-func (d *Dialer) Dial(_ context.Context, _ string) (net.Conn, error) {
-	return nil, nil
+func (d *Dialer) Dial(ctx context.Context, endpointID string) (net.Conn, error) {
+	// Dialing is simply opening a WebSocket connection to the target endpoint,
+	// then wrapping the WebSocket in a net.Conn.
+	return websocket.Dial(ctx, d.dialURL(endpointID))
+}
+
+func (d *Dialer) dialURL(endpointID string) string {
+	var dialURL url.URL
+	if d.URL == nil {
+		dialURL = url.URL{
+			Scheme: "http",
+			Host:   "localhost:8000",
+		}
+	} else {
+		dialURL = *d.URL
+	}
+
+	// Add the dial path to the URL.
+	dialURL.Path += "/_piko/v1/tcp/" + endpointID
+
+	// Set the scheme to WebSocket.
+	if dialURL.Scheme == "http" {
+		dialURL.Scheme = "ws"
+	}
+	if dialURL.Scheme == "https" {
+		dialURL.Scheme = "wss"
+	}
+
+	return dialURL.String()
 }
