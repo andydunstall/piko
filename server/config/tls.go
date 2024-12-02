7379e58eedc52c2bdaf96d312a383894acaa8084
Andy Dunstall <andydunstall@hotmail.co.uk>
2024-12-02 05:18:21 +0000

    tls: add mutual tls support (#195)

--- server/config/tls.go
+++ server/config/tls.go
@@ -2,14 +2,17 @@ package config
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
 
 	"github.com/spf13/pflag"
 )
 
 type TLSConfig struct {
-	Cert string `json:"cert" yaml:"cert"`
-	Key  string `json:"key" yaml:"key"`
+	Cert      string `json:"cert" yaml:"cert"`
+	Key       string `json:"key" yaml:"key"`
+	ClientCAs string `json:"client_cas" yaml:"client_cas"`
 }
 
 func (c *TLSConfig) Validate() error {
@@ -45,6 +48,16 @@ If given the server will listen on TLS`,
 		`
 Path to the PEM encoded key file.`,
 	)
+	fs.StringVar(
+		&c.ClientCAs,
+		prefix+"client-cas",
+		c.ClientCAs,
+		`
+A path to a certificate PEM file containing client certificiate authorities to
+verify the client certificates.
+
+When set the client must set a valid certificate during the TLS handshake.`,
+	)
 }
 
 func (c *TLSConfig) Load() (*tls.Config, error) {
@@ -58,6 +71,20 @@ func (c *TLSConfig) Load() (*tls.Config, error) {
 		return nil, fmt.Errorf("load key pair: %w", err)
 	}
 	tlsConfig.Certificates = []tls.Certificate{cert}
+
+	if c.ClientCAs != "" {
+		caCert, err := os.ReadFile(c.ClientCAs)
+		if err != nil {
+			return nil, fmt.Errorf("open client cas: %s: %w", c.ClientCAs, err)
+		}
+		caCertPool := x509.NewCertPool()
+		ok := caCertPool.AppendCertsFromPEM(caCert)
+		if !ok {
+			return nil, fmt.Errorf("parse client cas: %s: %w", c.ClientCAs, err)
+		}
+		tlsConfig.ClientCAs = caCertPool
+		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	}
 
 	return tlsConfig, nil
 }
