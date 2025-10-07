9b77fa3ce4bf2bd5b9bf0b38ac2e52ef3176b1df
Tiago Scolari <tiago@diagrid.io>
2025-10-07 09:50:02 +0100

    Add ClientTLSConfig to ProxyConfig

--- server/config/tls.go
+++ server/config/tls.go
@@ -92,3 +92,85 @@ func (c *TLSConfig) Load() (*tls.Config, error) {
 func (c *TLSConfig) enabled() bool {
 	return c.Cert != "" || c.Key != ""
 }
+
+type ClientTLSConfig struct {
+	Cert       string `json:"cert" yaml:"cert"`
+	Key        string `json:"key" yaml:"key"`
+	CAs        string `json:"cas" yaml:"cas"`
+	SkipVerify bool   `json:"skip_verify" yaml:"skip_verify"`
+}
+
+func (c *ClientTLSConfig) Load() (*tls.Config, error) {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: c.SkipVerify,
+	}
+
+	if c.CAs != "" {
+		caCertPool, err := x509.SystemCertPool()
+		if err != nil {
+			caCertPool = x509.NewCertPool()
+		}
+
+		caCert, err := os.ReadFile(c.CAs)
+		if err != nil {
+			return nil, fmt.Errorf("open cas: %s: %w", c.CAs, err)
+		}
+
+		ok := caCertPool.AppendCertsFromPEM(caCert)
+		if !ok {
+			return nil, fmt.Errorf("parse cas: %s: %w", c.CAs, err)
+		}
+
+		tlsConfig.RootCAs = caCertPool
+	}
+
+	if c.Cert == "" && c.Key == "" {
+
+		cert, err := tls.LoadX509KeyPair(c.Cert, c.Key)
+		if err != nil {
+			return nil, fmt.Errorf("load key pair: %w", err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
+	return tlsConfig, nil
+}
+
+func (c *ClientTLSConfig) RegisterFlags(fs *pflag.FlagSet, prefix string) {
+	prefix += ".client."
+
+	fs.StringVar(
+		&c.Cert,
+		prefix+"cert",
+		c.Cert,
+		`
+Path to the PEM encoded certificate file.
+
+Used for communication between Piko servers if mTLS is expected`,
+	)
+	fs.StringVar(
+		&c.Key,
+		prefix+"key",
+		c.Key,
+		`
+Path to the PEM encoded key file.`,
+	)
+	fs.StringVar(
+		&c.CAs,
+		prefix+"cas",
+		c.CAs,
+		`
+A path to a certificate PEM file containing certificiate authorities to
+verify the server certificates.
+
+Required when the server is using non-public certificates and not skip-verify.`,
+	)
+
+	fs.BoolVar(
+		&c.SkipVerify,
+		prefix+"skip-verify",
+		c.SkipVerify,
+		`
+Skip certificate verification between Piko servers.`,
+	)
+}
